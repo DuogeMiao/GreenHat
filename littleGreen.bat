5d5a04db0151cfb259d9afca492e9dd1d5230e2d
lizhi <1018771546@qq.com>
2024-03-10 11:19:06 +0800

    Greate

--- littleGreen.bat
+++ littleGreen.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d %~dp0
 git pull
-for /l %%i in (1, 1, 24) do (
+for /l %%i in (1, 1, 25) do (
 	echo console.log^(Hello Git [%%i] %time%^); > index.js
 	git add .
 	git commit -m 'Greate'
