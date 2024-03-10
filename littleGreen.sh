33bd5f1dfaa3d4a9f64ed885147203655f61ff3a
lizhi <1018771546@qq.com>
2024-03-10 11:18:25 +0800

    Greate

--- littleGreen.sh
+++ littleGreen.sh
@@ -1,12 +1,14 @@
 git pull
 #随机生成12-（12+18-1）的随机数 $(($RANDOM%(end-1)+start))
 count=$(($RANDOM%18+12))
+echo total\:$count
 for((var=0;var<$count;var++))
 do
 	echo var a = $var > index.js
 	git add .
 	git commit -m 'Greate'
 	git push
+	echo index\:$var
 done
 
 echo "*----------------SUCCESS--------------------*"
