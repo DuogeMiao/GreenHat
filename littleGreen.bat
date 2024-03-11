af99f84b6b570c5234ef541233187940f6f422f5
lizhi <zhi.li@houhoutech.com>
2024-03-11 13:56:13 +0800

    Greate

--- littleGreen.bat
+++ littleGreen.bat
@@ -1,16 +1,11 @@
 @echo off
-cd /d %~dp0
 git pull
 ::随机生成12-（12+18-1）的随机数count%%(end-1)+start
 set count=%random%
 set /a count=count%%18+12
 echo total:%count%
 for /l %%i in (1, 1, %count%) do (
-	echo console.log^(Hello Git [%%i] %time%^); > index.js
-	git add .
-	git commit -m 'Greate'
-	git push
-	echo index:%%i
+	echo %%i
 )
 echo *----------------SUCCESS--------------------*
 echo *                                           *
@@ -18,4 +13,5 @@ echo *                a commit a day             *
 echo *         keeps your girlfriend away        *
 echo *                                           *
 echo *-----------------SUCCESS-------------------*
-timeout /nobreak /t 10
+::timeout /nobreak /t 10
+pause
