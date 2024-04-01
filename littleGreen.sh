ef0382b213a8eca847c1ebc129ce284dbb9d1686
多哥喵 <1018771546@qq.com>
2024-04-01 02:35:01 +0000

    update littleGreen.sh.
    Signed-off-by: 多哥喵 <1018771546@qq.com>

--- littleGreen.sh
+++ littleGreen.sh
@@ -1,8 +1,9 @@
 #进入脚本的目录
 cd `dirname $0`
 git pull
-#随机生成12-（12+18-1）的随机数 $(($RANDOM%(end-1)+start))
-count=$(($RANDOM%18+12))
+#随机生成22 ~（22+19-1）的随机数,这个随机数落在[22,40]区间， $(($RANDOM%N+start)) => N=end+1-start
+#window的随机数生成和linux有差别
+count=$(($RANDOM%19+22))
 echo total\:$count
 for((var=0;var<$count;var++))
 do
